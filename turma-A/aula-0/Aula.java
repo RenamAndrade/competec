8f6679903765cbdbe63516e3ec20d27476f7a410
RenamAndrade <132757890+RenamAndrade@users.noreply.github.com>
2023-05-06 14:48:56 +0000

    aula0

---
+++ turma-A/aula-0/Aula.java
@@ -0,0 +1,105 @@
+import java.util.Scanner;
+
+public  class Aula {
+    
+    public static void main (String[] args) {
+        /*System.out.println("Teste");
+
+        //Ex 1
+
+        char[] Name = {'R', 'e', 'n', 'a', 'm'};
+        System.out.println(Name);
+
+        int Idade = 18;
+        System.out.println("Idade: " + Idade);
+
+        boolean cafe = true;*/
+
+        //Operadores
+        /*int idade = 18;
+        String nome = "Renam";
+        System.out.println(nome + ": Idade = " + idade);
+
+        int soma = (15 + 5);
+        System.out.println("Soma 1: " + soma);
+
+        long soma2 = (9999999 + 9999);
+        System.out.println("Soma 2: " + soma2);
+
+        int n1 = 45;
+        int n2 = 10;
+        int sub_result = (n1 - n2);
+        System.out.println("Sub 1: " + sub_result);
+
+        int n3 = 15;
+        int n4 = 5;
+        int mult_result = (n3 * n4);
+        System.out.println("Mult 1: " + mult_result);
+
+        double reau = 70 / 5.5;
+        System.out.println("Div 1: " + reau);
+        
+        int n5 = 32;
+        int n6 = 7;
+        double rest = (n5 % n6);
+        System.out.println("Rest 1: " + rest); */
+
+        //Laranja e Uva
+        /*double uva = 5.5;
+        double laranja = 10.95;
+        double soma = (uva + laranja);
+        System.out.println("Preço da Uva: " + uva  + "\nPreço da Laranja: " + laranja + "\nSoma dos preços: " + soma);*/
+
+        //Operações simples
+        /*int n1 = 50;
+        int n2 = 15;
+        n1 += n2;
+        System.out.println(n1);
+
+        int x = 45;
+        int y = 10;
+        x -= y;
+        System.out.println(x);
+        
+        int a = 9;
+        int b = 5;
+        a *= b;
+        System.out.println(a);
+
+        int c = 30;
+        int d = 3;
+        c /= d;
+        System.out.println(c);
+
+        int r = 55;
+        int e = 12;
+        r %= e;
+        System.out.println(r);*/
+
+        //Scanner
+        /*Scanner entrada = new Scanner(System.in);
+        System.out.println("Qual número você quer salvar?");
+        int numero = entrada.nextInt();
+        System.out.println("O número salvo foi " + numero);*/
+
+        //Tipos de Print
+        /*String saudacao = "Olá Mundo!";
+        System.out.print(saudacao "\n");*/
+    
+
+        //Exercício: Criar um Scanner pedindo o nome do usuário e sua idade
+        /*Scanner entradaN = new Scanner(System.in);
+        System.out.println("Qual o seu nome?");
+        String n = entradaN.nextLine();
+        System.out.println("Bom dia, " + n);
+
+        System.out.println("Qual sua idade?");
+        int i = entradaN.nextInt();
+        System.out.println("Sua idade é " + i);*/
+
+
+    }
+
+}
+    
+
